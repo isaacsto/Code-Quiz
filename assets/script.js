a93e52a7551c722ff5598e4ee640646a68764e16
Isaac Stofko <isaacstofko@gmail.com>
2023-02-15 09:28:47 -0500

    Initial Commit

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+
+// user sees "View High Scores" nav top left, time top right
+// header "Coding quiz challenge" with description
+// start button takes you to next page 
+// following pages have multiple choice questios
+// click button with chosen answer site alerts right or wrong and takes you to next page 
+// end of quiz enter initials, view high scores, clear high scores, go bak=ck option
+
+// trigger new page on click 
+// if answer correct alert right, if answer wrong alert wrong 
+
